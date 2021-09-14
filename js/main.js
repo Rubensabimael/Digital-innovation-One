6f2d20adb301138c435f3a8e9aba3a9717fd2fa9
rubensabimael <rubensabimael26@gmail.com>
2021-09-14 00:02:06 -0300

    com usar o variaveu.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var lista = ['maça', 'pêra','laranja'];
+//lista.push("uva");
+//lista.pop();
+/*console.log(lista);
+console.log(lista.toString());
+console.log(lista.join('|'));
+*/
+
+
+
+
+
+
+
+
+/*
+var nome = 'Rubens';
+var n1 = 17;
+var n2 = 10;
+var frase = 'Japão é o melho time da mudo';
+
+//alert(nome +'tem'+ idade+'anos');
+//alert(idade + idade2)
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace('Japão','Brasil'));
+alert((frase.replace('Japão','Brasil')))
+*/
